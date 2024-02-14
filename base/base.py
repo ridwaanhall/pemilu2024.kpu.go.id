ac38f37b584f2bc48ffe47cf7a28c8bc705cf97e
ridwaanhall <erbyl14@gmail.com>
2024-02-14 21:16:21 +0700

    Add option 4

--- base/base.py
+++ base/base.py
@@ -28,5 +28,26 @@ class OptionHandler:
                 print(f"Data fetched successfully from URL {option}!")
             else:
                 print(f"Failed to fetch data from URL {option}.")
+        elif option == "4":
+            self.fetch_and_display_data()
         else:
-            print("Invalid option. Please choose 1, 2, or 3.")
+            print("Invalid option. Please choose 1, 2, 3, or 4.")
+
+    def fetch_and_display_data(self):
+        url1 = "https://sirekap-obj-data.kpu.go.id/pemilu/ppwp.json"
+        url3 = "https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp.json"
+
+        data1 = DataFetcher.fetch_data_from_url(url1)
+        data3 = DataFetcher.fetch_data_from_url(url3)
+
+        if data1 and data3:
+            print(data3["ts"])
+            print(f"Progress: {data3['progres']['progres']} of {data3['progres']['total']} TPS ({data3['chart']['persen']}%)")
+
+            total_votes = sum(data3['chart'].values())
+            for key, value in data1.items():
+                percent = data3['chart'][key] / total_votes * 100
+                print(f"{value['nomor_urut']:02d}: {data3['chart'][key]:>10,} - {percent:.2f}% of total")
+        
+        else:
+            print("Failed to fetch data from one of the URLs.")
