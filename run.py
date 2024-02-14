a46702ebe8c972e07e0c5d77dcdf216ffa28ffb8
ridwaanhall <erbyl14@gmail.com>
2024-02-14 21:16:06 +0700

    Add option 4

--- run.py
+++ run.py
@@ -3,12 +3,13 @@ from base.base import OptionHandler
 if __name__ == "__main__":
     option_handler = OptionHandler()
 
-    print("Welcome to Data Fetcher!")
+    print("Welcome to pemilu 2024!")
     print("Choose an option:")
     print("1. Fetch data from URL 1")
     print("2. Fetch data from URL 2")
     print("3. Fetch data from URL 3")
+    print("4. Display data from URL 1 and URL 3")
 
-    option = input("Enter your choice (1, 2, or 3): ")
+    option = input("Enter your choice (1, 2, 3, or 4): ")
 
     option_handler.perform_option(option)
